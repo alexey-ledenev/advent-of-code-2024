8d9015e9b51f033fbd34f534e744ee164012cd21
alex <20566780+alexey-ledenev@users.noreply.github.com>
2024-12-02 00:17:16 +0300

    chore(main): run day file

--- src/main.ts
+++ src/main.ts
@@ -9,4 +9,25 @@ if (Number.isNaN(dayNumber)) {
   Deno.exit(1);
 }
 
-console.log(`Starting day ${dayNumber}...`);
+const filePath = `${Deno.cwd()}/src/day-${dayNumber}/index.ts`;
+
+try {
+  await Deno.lstat(filePath);
+} catch {
+  console.error(`Day ${dayNumber} not found`);
+  Deno.exit(1);
+}
+
+const command = new Deno.Command(Deno.execPath(), {
+  args: [filePath],
+});
+
+try {
+  console.log(`Starting day ${dayNumber}...`);
+
+  const child = command.spawn();
+  child.ref();
+} catch (error) {
+  console.error(`Error while running the day ${dayNumber}: `, error);
+  Deno.exit(4);
+}
